4dc4de4409e78c4e60d01b7b3fffe3b5e8f250f6
Maxim Molchanov <elzor.job@gmail.com>
2013-11-27 20:49:10 +0400

    add echo_cluster skeleton

---
+++ apps/echo_cluster/ebin/echo_cluster.app
@@ -0,0 +1,8 @@
+{application,echo_cluster,
+             [{description,[]},
+              {vsn,"1"},
+              {registered,[]},
+              {applications,[kernel,stdlib]},
+              {mod,{echo_cluster_app,[]}},
+              {env,[]},
+              {modules,[echo_cluster_app,echo_cluster_sup]}]}.
---
+++ apps/echo_cluster/src/echo_cluster_app.erl
@@ -0,0 +1,16 @@
+-module(echo_cluster_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    echo_cluster_sup:start_link().
+
+stop(_State) ->
+    ok.
