0f1900032737cb92d40253c6038971ccd9cb9247
Maxim Molchanov <elzor.job@gmail.com>
2013-11-28 23:46:01 +0400

    add start stop test

---
+++ apps/echo_cluster/test/start_stop_tests.erl
@@ -0,0 +1,35 @@
+-module(start_stop_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+-include_lib("common/include/test.hrl").
+
+-define(APPS, [ sasl ]).
+
+setup()->
+    error_logger:tty(false),
+    ?START_APPS(
+        ?APPS, [
+            {sasl, [ {sasl_error_logger, {file, atom_to_list(?MODULE)++".log"}} ]}
+            ]
+        ),
+    ok.
+
+
+cleanup(_)->
+    ?STOP_APPS(?APPS),
+    error_logger:tty(true),
+    ok.
+
+main_test_()->
+    ?FIXTURE(
+        fun()->
+            ?assertEqual(ok, application:start(echo_cluster)),
+            timer:sleep(75),
+            ?assertEqual(
+                {error,{already_started, echo_cluster}},
+                application:start(echo_cluster)
+                ),
+            ?assertEqual(ok, application:stop(echo_cluster)),
+            ok
+        end
+        ).
